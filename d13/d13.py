455eb85eb648f8d2afae21564581545025f4da47
Chao Duan <chao0duan@gmail.com>
2020-12-22 20:08:17 -0500

    day13 done

--- d13/d13.py
+++ d13/d13.py
@@ -3,8 +3,6 @@ import utils.read_input as inp
 lst = inp.to_origin_list()
 
 earliest = int(lst[0])
-timestamps = [int(n) for n in lst[1:][0].split(',') if n != 'x']
-timestamps.sort()
 
 
 def lowest_greater(earliest, timestamps):
@@ -22,9 +20,29 @@ def lowest_greater(earliest, timestamps):
     return smallest_no_smaller, bus_id
 
 
+def earliest_timestamp(timestamps, minutes):
+    time, step = 0, 1
+    for i in range(len(minutes)):
+        t, m = timestamps[i], minutes[i]
+        while (time + m) % t != 0:
+            time += step
+        step *= t
+    return time
+
+
 if __name__ == "__main__":
-    time_stamp, bus_id = lowest_greater(earliest, timestamps)
+    time_stamp, bus_id = lowest_greater(earliest, sorted([int(n) for n in
+                                        lst[1:][0].split(',') if n != 'x']))
     if time_stamp == 0:
         print('Part 1: 0')
     else:
         print(f'Part 1: {(time_stamp - earliest) * bus_id}')
+
+    timestamps = []
+    minutes = []
+    for i, n in enumerate(lst[1:][0].split(',')):
+        if n != 'x':
+            timestamps.append(int(n))
+            minutes.append(i)
+
+    print(f'Part 2: {earliest_timestamp(timestamps, minutes)}')
